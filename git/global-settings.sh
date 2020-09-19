a00447ead19848ce3683b3819e626ba1078624d7
George Zagaris <george.zagaris@gmail.com>
2020-09-19 15:58:01 -0700

    ENH: add global git setting script

---
+++ git/global-settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+##======================================================
+#
+# This script is used to setup git user-settings
+#
+##======================================================
+
+git config --global push.default current
