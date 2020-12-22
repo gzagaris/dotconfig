bd2d552de77e5f13324eccb79c2480690ddaa4b7
George Zagaris <george.zagaris@gmail.com>
2020-12-22 18:46:20 -0500

    ENH: update global Git settings

--- git/global-settings.sh
+++ git/global-settings.sh
@@ -2,8 +2,22 @@
 
 ##======================================================
 #
-# This script is used to setup git user-settings
+# This script is used to setup global git user-settings
+# across all Git projects.
 #
 ##======================================================
 
+## `git push` if given no other argument will push the
+## current branch to a remote branch with the same name.
 git config --global push.default current
+
+## enable syntax highlighting of output from git commands.
+git config --global color.ui true
+
+## enable rebasing on `git pull`.
+git config --global pull.rebase true
+
+## enable ``rerere`` functionality for conflict resolution
+## which allows to [re]use [re]corded [re]solution. Simplifies
+## life when rebasing and resolving conflicts.
+git config --global rerere.enabled true
